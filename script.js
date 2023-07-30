c99ceb4b84c63450d6618a6d5861714a8d5f0326
bennetcodes <83713963+penheiros@users.noreply.github.com>
2023-07-31 01:46:52 +0600

    Implemented New Timezones (Non-functional)
    - New "Change Timezone" Button
    - New dialog modal popup for timezone options.

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 var hour12 = true
 
 setInterval(function displayTime() {
-    var dateAndTime = new Date().toLocaleString('en-US', {hour12 : hour12, timeStyle: 'full'});
-    var time = dateAndTime.split(' ')[0];
-    var amOrPm = dateAndTime.split(' ')[1];
+
+    var dateTime = new Date().toLocaleString('en-US', {hour12 : hour12, timeStyle: 'full'});
+    var time = dateTime.split(' ')[0];
+    var amOrPm = dateTime.split(' ')[1];
+
     var timeDisplay = document.querySelector('#time');
     var timezoneDisplay = document.querySelector('#timezone')
     
-    console.log(amOrPm)
+    
     timeDisplay.innerHTML = `${time} ${amOrPm=='AM'||amOrPm=='PM' ? amOrPm : ''}`;
-    timezoneDisplay.innerHTML = `${dateAndTime.split(' ')[2]} ${dateAndTime.split(' ')[3]}`;
+    timezoneDisplay.innerHTML = `${dateTime.split(' ')[2]} ${dateTime.split(' ')[3]}`;
+
 }, 1000)
 
+// Switching between military and 12 hour time
 toggleButton = document.querySelector('#toggle-time');
 document.querySelector('#toggle-time').addEventListener('click', ()=> {
     toggleState = toggleButton.className;
@@ -26,6 +30,7 @@ document.querySelector('#toggle-time').addEventListener('click', ()=> {
     }
 })
 
+// Handling background changes
 var backgrounds = {
     'red' : 'linear-gradient(lightcoral, lightcoral)',
     'blue': 'linear-gradient(rgba(143,194,255,1), rgba(143,194,255,1))',
@@ -42,3 +47,23 @@ document.querySelector('#background-change').addEventListener('click', ()=> {
     if (backgroundChangeCount == backgroundKeys.length - 1) {backgroundChangeCount = 0;} 
     else {backgroundChangeCount++;}
 })
+
+// Handling timezone changes
+
+var timezoneButton = document.querySelector('#timezone-change');
+var timezoneOptions = document.querySelector('#timezone-options');
+var timezoneExit = document.querySelector('#exit-button');
+var timezoneContainer = document.querySelector('#timezone-container');
+timezoneButton.addEventListener('click', ()=> {timezoneOptions.showModal();})
+timezoneExit.addEventListener('click', ()=> {timezoneOptions.close();})
+
+
+fetch('timezones.json')
+    .then(response => response.json())
+    .then(data => {
+        data.map(timezone => {
+            var option = document.createElement('button');
+            timezoneContainer.appendChild(option);
+            option.innerHTML = timezone.timezone;
+        })
+    })
